21228f8352b1abcae50409c8443c7f528d4f08b1
JustinMatthewNewman <newma4jm@dukes.jmu.edu>
2023-02-22 03:17:59 +0000

    Update

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define('db_host','localhost');
 // database username
 define('db_user','root');
 // database password
-define('db_pass','Jmu4815162342!');
+define('db_pass','Jmupassword1!');
 // database name
 define('db_name','brightspot');
 // database charset, change this only if utf8 is not supported by your language
@@ -13,7 +13,7 @@ define('db_charset','utf8');
 // Admin username
 define('admin_user','Brightspot');
 // Admin password
-define('admin_pass','Jmu4815162342!');
+define('admin_pass','Jmupassword1!');
 // Approval required for comments?
 define('comments_approval_required',true);
 // Default profile image
